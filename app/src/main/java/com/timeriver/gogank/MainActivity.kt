82ce3fab0053f0eecd92c7e5f6b5e270fbf6ac22
xibei sunny <xibeisunny@xibeideMacBook-Pro.local>
2019-04-30 11:38:45 +0800

    Test Kotlin Coroutines

--- app/src/main/java/com/timeriver/gogank/MainActivity.kt
+++ app/src/main/java/com/timeriver/gogank/MainActivity.kt
@@ -2,11 +2,26 @@ package com.timeriver.gogank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+/**
+ * 使用Kotlin Coroutines替代RxJava实现异步请求
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Hello MainActivity!")
+        val launch = GlobalScope.launch {
+            println("Hello Kotlin Coroutines!")
+        }
+
+        runBlocking {
+            launch.join()
+        }
     }
 }
